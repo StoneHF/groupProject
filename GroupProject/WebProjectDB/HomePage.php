977192ce0d85c315c24f643762638913b2b09ecf
DylanGreenwell <97881207+DylanGreenwell@users.noreply.github.com>
2023-03-13 12:47:44 +0000

    Update HomePage.php
    Added session code to the home page.

---
+++ GroupProject/WebProjectDB/HomePage.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+include 'config/connn.php';
+include 'loginNavbar/loginNavbarr.php';
+
+$varMemberID = $_SESSION['fldMemberID'];
+
+if (isset($_SESSION['fldMemberID'])){
+    echo 'User is logged in with fldMemberID' . $_SESSION['fldMemberID'];
+}
+else{
+    echo 'User is not logged in';
+}
+
+?>
