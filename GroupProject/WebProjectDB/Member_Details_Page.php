44ce25c4e2773ea2ff65096828e6c37387bc2ae4
StoneHF <94833939+StoneHF@users.noreply.github.com>
2023-04-12 16:21:55 +0100

    Update Member_Details_Page.php
    Made some changes to the layout of the page. Members now have the option to delete their account once selected. A modal pops up asking the member to confirm they want their account deleted or if they want to cancel the rpocess.

--- GroupProject/WebProjectDB/Member_Details_Page.php
+++ GroupProject/WebProjectDB/Member_Details_Page.php
@@ -8,9 +8,46 @@
 <html>
 <head>
 
-<style>
 
-      /* define styles here */
+<link rel="stylesheet" type="text/css" href="CSS/DeleteAccountModal.css">
+
+<style>
+  /* define styles here */
+  button#confirmDeleteModalBtn {
+  display: block;
+  margin: 0 auto;
+}
+
+
+/* Style for the modal container */
+.modal {
+    display: none;
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgba(0, 0, 0, 0.4);
+  }
+
+  /* Style for the modal content */
+  .modal-content {
+    background-color: #fefefe;
+    margin: 15% auto;
+    padding: 20px;
+    border: 1px solid #888;
+    width: 80%;
+    max-width: 400px;
+    text-align: center;
+  }
+
+  #confirmDeleteModal button:first-child {
+    width: calc(50% - 5px);
+  }
+
+      
 </style>
 
 
@@ -68,10 +105,43 @@ while($row = $result->fetch_assoc()) {
     echo "</tr>";
 }
 
+
 echo "</table>";
 echo "</div>";
 
 ?>
+
+<form action="process_member_details.php" method="post">
+<button type="button" class="btn btn-danger" id="confirmDeleteModalBtn">Delete Account</button>
+
+<!-- The modal container -->
+<div id="confirmDeleteModal" class="modal">
+  <!-- Modal content -->
+  <div class="modal-content">
+    <h2>Confirm Account Deletion</h2>
+    <p>Are you sure you want to delete your account? This action cannot be undone.</p>
+    <form action="process_member_details.php" method="post">
+      <button type="submit" name="process_member_details">Yes, Delete Account</button>
+      <button type="button" onclick="closeDeleteModal()">Cancel</button>
+    </form>
+  </div>
+</div>
+
+<!-- JavaScript to handle opening and closing the modal -->
+<script>
+document.getElementById("confirmDeleteModalBtn").addEventListener("click", function(){
+  document.getElementById("confirmDeleteModal").style.display = "block";
+});
+
+function closeDeleteModal() {
+  document.getElementById("confirmDeleteModal").style.display = "none";
+}
+</script>
+
+
+</form>
+
+
 
 </body>
 </html>
