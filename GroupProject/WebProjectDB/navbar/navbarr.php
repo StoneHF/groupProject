e1a8e0d95f2c5dc144fb95934f793a0e2fabbc16
StoneHF <94833939+StoneHF@users.noreply.github.com>
2023-04-12 15:36:42 +0100

    Update navbarr.php
    added additional Hrefs

--- GroupProject/WebProjectDB/navbar/navbarr.php
+++ GroupProject/WebProjectDB/navbar/navbarr.php
@@ -8,7 +8,7 @@
 
 
 
-<title>Harry's Assignment Website</title>
+<title>GNN project</title>
 <!--  BOOTSTRAP CSS CDN -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 <link rel="stylesheet" href="css/bootstrap.css">
@@ -54,7 +54,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"> Gaming News Now GNN</a>
+  <a class="navbar-brand" href="HomePage.php">Gaming News Now GNN</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -65,6 +65,12 @@
         </li>
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="upcoming_game_gallery.php">Upcoming Games</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="game_voting_form.php">Vote On Your Favourite Games</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="competition_results_gallery.php">Most Voted For Games</a>
         </li>
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="Member_Details_Page.php"> Your Details</a>
