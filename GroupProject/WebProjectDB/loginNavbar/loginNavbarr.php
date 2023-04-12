0ff379216242626c633d53ff779d0b9dbc77dd63
StoneHF <94833939+StoneHF@users.noreply.github.com>
2023-04-12 15:34:46 +0100

    Update loginNavbarr.php
    Added Href so users can navigate to home page just from clicking on GNN

--- GroupProject/WebProjectDB/loginNavbar/loginNavbarr.php
+++ GroupProject/WebProjectDB/loginNavbar/loginNavbarr.php
@@ -6,7 +6,7 @@
 
 
 
-<title>H&D Project Assignment</title>
+<title>GNNproject</title>
 <!--  BOOTSTRAP CSS CDN -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 <link rel="stylesheet" href="css/bootstrap.css">
@@ -25,7 +25,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"> Gaming News Now GNN</a>
+  <a class="navbar-brand" href="HomePage.php">Gaming News Now GNN</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
