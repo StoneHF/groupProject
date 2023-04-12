0deaf05de3d1a80142973b3ef836640ae1a528a2
StoneHF <94833939+StoneHF@users.noreply.github.com>
2023-04-12 15:30:40 +0100

    Update navbarCheck.php
    Added a new navigation bar for users who are not members or admins, only displays the pages they are allowed to access.

--- GroupProject/WebProjectDB/adminCheck/navbarCheck.php
+++ GroupProject/WebProjectDB/adminCheck/navbarCheck.php
@@ -1,8 +1,10 @@
 <?php
 if (isset($_SESSION['isadmin']) && $_SESSION['isadmin'] == 1) {
     include 'AdminNav/adNavbar.php';
+} else if (isset($_SESSION['fldMemberID'])) {
+    include 'navbar/navbarr.php';
 } else {
-     include 'navbar/navbarr.php';
+    include 'NonMemberNavbar/NonMemNav.php';
 }
 ?>
 
