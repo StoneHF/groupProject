fc9bedd4074cb783117cad8e607135e28076ceba
DylanGreenwell <97881207+DylanGreenwell@users.noreply.github.com>
2023-04-25 23:29:26 +0100

    Update admin_home_page.php
    Updated the page slightly.

--- GroupProject/WebProjectDB/admin_home_page.php
+++ GroupProject/WebProjectDB/admin_home_page.php
@@ -3,6 +3,28 @@ session_start();
 include 'config/connn.php';
 include 'adminCheck/navbarCheck.php';
 
-$varMemberID = $_SESSION['fldMemberID'];
+if (isset($_SESSION['fldMemberID'])){
+    //echo 'User is logged in with fldMemberID' . $_SESSION['fldMemberID']; displays member id
+}
+ else{
+    //echo 'User is not logged in go away '; testing purposes - if user isnt logged in
+}
 
 ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <style>
+        #text-header{
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <h1 id="text-header">Welcome, Admin! Please use the Navbar to navigate to Admin Pages...</h1>
+</body>
+
+</html>
