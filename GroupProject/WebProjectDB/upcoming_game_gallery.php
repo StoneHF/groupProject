6eb43850cb437d3467cb85b204237c110e89e499
StoneHF <94833939+StoneHF@users.noreply.github.com>
2023-04-27 00:30:50 +0100

    Update upcoming_game_gallery.php
    removed some undeeded code and added comments

--- GroupProject/WebProjectDB/upcoming_game_gallery.php
+++ GroupProject/WebProjectDB/upcoming_game_gallery.php
@@ -3,78 +3,49 @@ session_start();
 include 'config/connn.php';
 include 'adminCheck/navbarCheck.php';
 
-// Check if the fldMemberID session variable is set
-if (isset($_SESSION['fldMemberID'])) {
-  // The fldMemberID session variable is set, so the user is logged in
- // echo 'User is logged in with fldMemberID: ' . $_SESSION['fldMemberID'];
-} else {
-  // The fldMemberID session variable is not set, so the user is not logged in
-  echo 'User is not logged in';
-  header ('Location: loginn_form.php');
-}
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
 
 <html>
 <head>
+<link rel="stylesheet" type="text/css" href="css/projectWebsiteStyle.css">
 <style>
-.gallery {
-  margin: 5px;
-  border: 1px solid black;
-  width: calc(50% - 10px); /* 3 items in a row with 10px gap */
-}
-
-
-
 .gallery-container {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-gap: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
   width: 90%; /* set a fixed width */
   margin: 0 auto; /* center the container */
-  padding-left: 190px;
-  padding-right: 1px;
 }
 
-/* Styles for tablets */
-@media only screen and (max-width: 768px) {
-  .gallery-container {
-    grid-template-columns: repeat(2, 1fr);
-  }
+.gallery {
+  margin: 20px;
+  text-align: center;
+  border: 1px solid #ccc;
+  padding: 10px;
+  width: 300px;
 }
-
-/* Styles for mobile */
-@media only screen and (max-width: 480px) {
-  .gallery-container {
-    grid-template-columns: 1fr;
-  }
-}
-
 
 .gallery:hover {
   border: 1px solid #777;
 }
 
 .gallery img {
-  display: block;
-  width: 100%;
+  max-width: 100%;
+  max-height: 300px;
   object-fit: cover;
   object-position: center;
 }
 
 .desc {
-  padding: 15px;
+  margin-top: 10px;
   text-align: center;
-}
-
-.asc-title {
   font-weight: bold;
+  font-size: 1.2em;
 }
 
-
-body {
-  text-align: center;
-}
 </style>
 
 
@@ -82,13 +53,16 @@ body {
 <body>
 
 
+<!-- Create the gallery container -->
 <div class="gallery-container">
 <?php
-// for($x=0; $x<=5; $x++)
+// Select all images from the tbl_images table and order them by release date in ascending order
 $sql = "SELECT * FROM `tbl_images` ORDER BY `fldReleaseDate` ASC";
 
+// Execute the SQL query and store the result in a variable
 $result = $mysqli->query($sql);
 
+// Loop through each row returned by the query and display the image and its details in a gallery div
 while ($row = $result->fetch_assoc()) {
     echo '<div class="gallery">';
     echo '<a target="_blank" href="' . $row['fldPath'] . '">';
@@ -96,15 +70,19 @@ while ($row = $result->fetch_assoc()) {
     echo '</a>';
     echo '<div class="desc"><span class="asc-title">' . $row['fldImageTitle'] . '</div>';
     echo '<div class="desc"><span class="asc-title">Platforms:';
+
+    // Check which platforms the image is available on and display them
     if ($row['fldXbox'] == 1) {
-        echo ' Xbox';
+        echo ' Xbox,';
     }
     if ($row['fldPlaystation'] == 1) {
-        echo ' Playstation';
+        echo ' Playstation,';
     }
     if ($row['fldPC'] == 1) {
         echo ' PC';
     }
+
+    // Display the release date for the image
     echo '</div>';
     echo '<div class="desc"><span class="asc-title">Release Date: ' . $row['fldReleaseDate'] . '</div>';
     echo '</div>';
